53c997f638f84c9bdadde01d4bd3b86ffc98c938
villa <matias.villalemes@metropolia.fi>
2024-08-23 10:12:31 +0300

    harj 1 tehty

---
+++ 1_kuukausi.py
@@ -0,0 +1,4 @@
+vuodenajat = ("Talvi","Talvi","Talvi","Keväät","Keväät","Keväät","Kesä","Kesä","Kesä","Syysky","Syysky","Syysky",)
+kuukausi = int(input("Anna kuukauden numero: "))
+
+print(vuodenajat[kuukausi-1])
